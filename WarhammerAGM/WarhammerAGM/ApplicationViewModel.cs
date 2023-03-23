a78798331540a2009311ee7d2af922e41286c196
EldHasp <eldhasp@gmail.com>
2023-03-23 22:20:02 +0300

    Реализовано добавление и удаление.

--- WarhammerAGM/WarhammerAGM/ApplicationViewModel.cs
+++ WarhammerAGM/WarhammerAGM/ApplicationViewModel.cs
@@ -5,34 +5,22 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using ViewModels;
 using WarhammerAGM.Models;
 using Xceed.Wpf.Toolkit;
 
 namespace WarhammerAGM
 {
-    public class ApplicationViewModel
+    public class ApplicationViewModel : ViewModelBase
     {
-        ApplicationContext db = new ApplicationContext();
-        RelayCommand? addCommand;
-        RelayCommand? editCommand;
-        RelayCommand? deleteCommand;
-        public ObservableCollection<BestiaryCreature> BestiaryCreatures { get; set; }
+       private readonly  ApplicationContext db = new ApplicationContext();
 
-        private BestiaryCreature _bestiaryCreature = new();
+        public ObservableCollection<BestiaryCreature> BestiaryCreatures { get; }
+
         public BestiaryCreature BestiaryCreature
         {
-            get => _bestiaryCreature;
-            private set
-            {
-                _bestiaryCreature = value;
-                OnPropertyChanged("BestiaryCreature");
-            }
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            get => Get<BestiaryCreature>()!;
+            private set => Set(value ?? throw new ArgumentNullException(nameof(value)));
         }
 
         public ApplicationViewModel()
@@ -40,52 +28,46 @@ namespace WarhammerAGM
             /*В конструкторе класса загружаем данные из бд в локальный кэш*/
             db.Database.EnsureCreated();
             db.BestiaryCreatures.Load();
+            BestiaryCreature = new();
             BestiaryCreatures = db.BestiaryCreatures.Local.ToObservableCollection();
-        }  
-        // команда добавления
-        public RelayCommand AddCommand
-        {
-            get
-            {
-                return addCommand ??
-                  (addCommand = new RelayCommand((o) =>
-                  {
-
-                      db.BestiaryCreatures.Add(BestiaryCreature);
-                      db.SaveChanges();
-                      BestiaryCreature = new BestiaryCreature();
-                      MessageBox.Show("Добавление прошло успешно");
-                  }));
-            }
         }
-       /*private void ClearSpecFields()
+        // команда добавления
+        public RelayCommand AddCommand => GetCommand(() =>
         {
-            if (BestiaryCreature.Name != null || BestiaryCreature.Description != null || BestiaryCreature.Melee != null || BestiaryCreature.Ballistics != null || BestiaryCreature.Power != null
-                || BestiaryCreature.Endurance != null || BestiaryCreature.Dexterity != null || BestiaryCreature.Intelligence != null || BestiaryCreature.Perception != null || BestiaryCreature.Willpower != null || BestiaryCreature.Partnership != null || BestiaryCreature.Wounds != null
-                || BestiaryCreature.Speed != null || BestiaryCreature.Skills != null || BestiaryCreature.Talents != null || BestiaryCreature.Armor != null || BestiaryCreature.Weapons != null || BestiaryCreature.Equipment != null || BestiaryCreature.Features != null || BestiaryCreature.AdditionalFeatures != null)
-            {
-                BestiaryCreature.Name = string.Empty;
-                BestiaryCreature.Description = string.Empty;
-                BestiaryCreature.Melee = null;
-                BestiaryCreature.Ballistics = null;
-                BestiaryCreature.Power = null;
-                BestiaryCreature.Endurance = null;
-                BestiaryCreature.Dexterity = null;
-                BestiaryCreature.Intelligence = null;
-                BestiaryCreature.Perception = null;
-                BestiaryCreature.Willpower = null;
-                BestiaryCreature.Partnership = null;
-                BestiaryCreature.Wounds = null;
-                BestiaryCreature.Speed = string.Empty;
-                BestiaryCreature.Skills = string.Empty;
-                BestiaryCreature.Talents = string.Empty;
-                BestiaryCreature.Armor = string.Empty;
-                BestiaryCreature.Weapons = string.Empty;
-                BestiaryCreature.Equipment = string.Empty;
-                BestiaryCreature.Features = string.Empty;
-                BestiaryCreature.AdditionalFeatures = string.Empty;
-            }
-        }*/
+            db.BestiaryCreatures.Add(BestiaryCreature);
+            db.SaveChanges();
+            BestiaryCreature = new();
+            MessageBox.Show("Добавление прошло успешно");
+        });
+
+        /*private void ClearSpecFields()
+         {
+             if (BestiaryCreature.Name != null || BestiaryCreature.Description != null || BestiaryCreature.Melee != null || BestiaryCreature.Ballistics != null || BestiaryCreature.Power != null
+                 || BestiaryCreature.Endurance != null || BestiaryCreature.Dexterity != null || BestiaryCreature.Intelligence != null || BestiaryCreature.Perception != null || BestiaryCreature.Willpower != null || BestiaryCreature.Partnership != null || BestiaryCreature.Wounds != null
+                 || BestiaryCreature.Speed != null || BestiaryCreature.Skills != null || BestiaryCreature.Talents != null || BestiaryCreature.Armor != null || BestiaryCreature.Weapons != null || BestiaryCreature.Equipment != null || BestiaryCreature.Features != null || BestiaryCreature.AdditionalFeatures != null)
+             {
+                 BestiaryCreature.Name = string.Empty;
+                 BestiaryCreature.Description = string.Empty;
+                 BestiaryCreature.Melee = null;
+                 BestiaryCreature.Ballistics = null;
+                 BestiaryCreature.Power = null;
+                 BestiaryCreature.Endurance = null;
+                 BestiaryCreature.Dexterity = null;
+                 BestiaryCreature.Intelligence = null;
+                 BestiaryCreature.Perception = null;
+                 BestiaryCreature.Willpower = null;
+                 BestiaryCreature.Partnership = null;
+                 BestiaryCreature.Wounds = null;
+                 BestiaryCreature.Speed = string.Empty;
+                 BestiaryCreature.Skills = string.Empty;
+                 BestiaryCreature.Talents = string.Empty;
+                 BestiaryCreature.Armor = string.Empty;
+                 BestiaryCreature.Weapons = string.Empty;
+                 BestiaryCreature.Equipment = string.Empty;
+                 BestiaryCreature.Features = string.Empty;
+                 BestiaryCreature.AdditionalFeatures = string.Empty;
+             }
+         }*/
         // команда редактирования
         /*public RelayCommand EditCommand
         {
@@ -114,22 +96,12 @@ namespace WarhammerAGM
         }
        */
         // команда удаления
-        public RelayCommand DeleteCommand
+        public RelayCommand DeleteCommand => GetCommand<BestiaryCreature>(selectedItem =>
         {
-            get
-            {
-                return deleteCommand ??
-                  (deleteCommand = new RelayCommand((selectedItem) =>
-                  {
-                      // получаем выделенный объект
-                      BestiaryCreature? bestiaryCreature = selectedItem as BestiaryCreature;
-                      if (bestiaryCreature == null) return;
-                      db.BestiaryCreatures.Remove(bestiaryCreature);
-                      db.SaveChanges();
-                  }));
-            }
-        }
+            db.BestiaryCreatures.Remove(selectedItem);
+            db.SaveChanges();
+        });
 
-        public object? Wounds { get; private set; }
+        public object? Wounds { get => Get<object>(); private set => Set(value); }
     }
 }
